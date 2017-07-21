99284209d1cf1e20d789559b5f74e3ad493e5861
Jason <quietjason@gmail.com>
2017-07-21 16:30:16 +0800

    Could convert Fahrenheit to Celsius.

--- 3 - View/Solutions/WorldTrotter/WorldTrotter/ViewController.swift
+++ 3 - View/Solutions/WorldTrotter/WorldTrotter/ViewController.swift
@@ -12,23 +12,49 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var fahrenheitTextField: UITextField!
   @IBOutlet weak var celsiusLabel: UILabel!
+  
+  var fahrenheitValue: Measurement<UnitTemperature>? {
+    didSet {
+      updateCelsius()
+    }
+  }
+  
+  var celsiusValue: Measurement<UnitTemperature>? {
+    if let fahrenheitValue = fahrenheitValue {
+      return fahrenheitValue.converted(to: .celsius)
+    } else {
+      return nil
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    celsiusLabel.text = "???"
+    updateCelsius()
   }
   
+  // MARK: - Action
+  
   @IBAction func fahrenheitChanged(_ sender: UITextField) {
-    if let text = sender.text {
-      celsiusLabel.text = text
+    if let text = sender.text, let value = Double(text) {
+      fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
     } else {
-      celsiusLabel.text = "???"
+      fahrenheitValue = nil
     }
   }
   
   @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
     fahrenheitTextField.resignFirstResponder()
+  }
+  
+  // MARK: - Helper
+  
+  func updateCelsius() {
+    if let celsiusValue = celsiusValue {
+      celsiusLabel.text = "\(celsiusValue.value)"
+    } else {
+      celsiusLabel.text = "???"
+    }
   }
 }
 

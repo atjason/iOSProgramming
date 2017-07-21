b47466c1dbc53d59d501e967c3ebacd6a917f4a4
Jason <quietjason@gmail.com>
2017-07-21 17:38:33 +0800

    Let Fahrenheit text field get first responder when app starts.

--- 3 - View/Solutions/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ 3 - View/Solutions/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -39,6 +39,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     super.viewDidLoad()
     
     updateCelsius()
+    
+    fahrenheitTextField.becomeFirstResponder()
   }
   
   // MARK: - Action

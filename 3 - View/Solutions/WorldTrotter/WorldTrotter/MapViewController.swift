b53e20d47bf7863cd7bc6077862255a4a96f6ec3
Jason <quietjason@gmail.com>
2017-07-21 21:43:07 +0800

    Add segmented control.

--- 3 - View/Solutions/WorldTrotter/WorldTrotter/MapViewController.swift
+++ 3 - View/Solutions/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -17,7 +17,13 @@ class MapViewController: UIViewController {
     super.loadView()
     
     mapView = MKMapView()
-    
     view = mapView
+    
+    let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+    segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    segmentedControl.selectedSegmentIndex = 0
+    
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(segmentedControl)
   }
 }

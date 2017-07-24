1abcb308a0adebe348f1e04ce6cfb82f7d475e20
Jason <quietjason@gmail.com>
2017-07-24 15:47:36 +0800

    Add class ItemCell.

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jason Zheng on 2017/07/24.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var serialNumberLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+}

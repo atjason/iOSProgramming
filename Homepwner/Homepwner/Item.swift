41984bbfb1c2189f54a106cd42efcbb5dfae079f
Jason <quietjason@gmail.com>
2017-07-24 10:47:46 +0800

    Add convenience init for Item.

--- Homepwner/Homepwner/Item.swift
+++ Homepwner/Homepwner/Item.swift
@@ -22,4 +22,32 @@ class Item: NSObject {
     
     super.init()
   }
+  
+  convenience init(random: Bool) {
+    guard random else {
+      self.init(name: "", price: 0)
+      return
+    }
+    
+    let owners = ["Tom", "John", "Emily"]
+    let objects = ["House", "Car", "Computer", "Phone"]
+    
+    let ownerIndex = Item.randomInt(upper: owners.count)
+    let owner = owners[ownerIndex]
+    
+    let objectIndex = Item.randomInt(upper: objects.count)
+    let object = objects[objectIndex]
+    
+    let name = "\(owner)'s \(object)"
+    
+    let price = Item.randomInt(upper: 100)
+    
+    let serialNumber = UUID().uuidString.components(separatedBy: "-").first!
+    
+    self.init(name: name, price: price, serialNumber: serialNumber)
+  }
+  
+  static func randomInt(upper: Int) -> Int {
+    return Int(arc4random_uniform(UInt32(upper)))
+  }
 }
--- Homepwner/Homepwner/ItemStore.swift
+++ Homepwner/Homepwner/ItemStore.swift
@@ -15,34 +15,7 @@ class ItemStore {
     items = [Item]()
     
     for _ in 0..<5 {
-      items.append(createItem(random: true))
+      items.append(Item(random: true))
     }
-  }
-  
-  @discardableResult func createItem(random: Bool) -> Item {
-    guard random else {
-      return Item(name: "", price: 0)
-    }
-    
-    let owners = ["Tom", "John", "Emily"]
-    let objects = ["House", "Car", "Computer", "Phone"]
-    
-    let ownerIndex = randomInt(upper: owners.count)
-    let owner = owners[ownerIndex]
-    
-    let objectIndex = randomInt(upper: objects.count)
-    let object = objects[objectIndex]
-    
-    let name = "\(owner)'s \(object)"
-    
-    let price = randomInt(upper: 100)
-    
-    let serialNumber = UUID().uuidString.components(separatedBy: "-").first!
-    
-    return Item(name: name, price: price, serialNumber: serialNumber)
-  }
-  
-  func randomInt(upper: Int) -> Int {
-    return Int(arc4random_uniform(UInt32(upper)))
   }
 }

982c69bc7519bba26fbc18bc02be9977080a1b1d
Jason <quietjason@gmail.com>
2017-07-25 10:33:05 +0800

    class DetailViewController

---
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Homepwner
+//
+//  Created by Jason Zheng on 2017/07/25.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+}

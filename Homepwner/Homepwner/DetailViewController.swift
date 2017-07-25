9270d0bed27e4e31d9ecad394ab300b7c556129b
Jason <quietjason@gmail.com>
2017-07-25 10:46:11 +0800

    Add outlet and load info when view appears.

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class DetailViewController: UIViewController {
   
+  @IBOutlet weak var nameField: UITextField!
+  @IBOutlet weak var serialField: UITextField!
+  @IBOutlet weak var priceField: UITextField!
+  @IBOutlet weak var dateLabel: UILabel!
+  
+  var item: Item!
+  
+  let priceFormater: NumberFormatter = {
+    let formater = NumberFormatter()
+    
+    formater.numberStyle = .decimal
+    formater.minimumFractionDigits = 2
+    formater.maximumFractionDigits = 2
+    
+    return formater
+  }()
+  
+  let dateFormater: DateFormatter = {
+    let formater = DateFormatter()
+    
+    formater.dateStyle = .medium
+    formater.timeStyle = .none
+    
+    return formater
+  }()
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    nameField.text = item.name
+    serialField.text = item.serialNumber
+    priceField.text = priceFormater.string(from: NSNumber(value: item.price))
+    dateLabel.text = dateFormater.string(from: item.date)
+  }
 }

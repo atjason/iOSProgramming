48264a72d152257e52df3f7428b55fb6c82cf7e0
Jason <quietjason@gmail.com>
2017-07-25 10:50:11 +0800

    Could show detail of an Item.

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -42,6 +42,19 @@ class ItemsViewController: UITableViewController {
     }
   }
   
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    switch segue.identifier {
+    case "showItem"?:
+      if let row = tableView.indexPathForSelectedRow?.row,
+        let detailViewController = segue.destination as? DetailViewController {
+        detailViewController.item = itemStore.items[row]
+      }
+      
+    default:
+      preconditionFailure("Invalid segue identifier.")
+    }
+  }
+  
   // MARK: - UITableViewDataSource
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {

d05339341ae72c29bedf457c836792d6b6bd9d18
Jason <quietjason@gmail.com>
2017-07-26 16:47:32 +0800

    Add struct Line.

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Jason Zheng on 2017/07/26.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+  var start = CGPoint()
+  var end = CGPoint()
+}

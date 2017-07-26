0dd4cc6f2ac3c25ab5b06ef8dd3eb67b41ddc381
Jason <quietjason@gmail.com>
2017-07-26 13:32:43 +0800

    Watch App's life cycle.

--- Homepwner/Homepwner/AppDelegate.swift
+++ Homepwner/Homepwner/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   let itemStore = ItemStore()
 
+  func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    print(#function)
+    return true
+  }
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    print(#function)
+    
     let imageStore = ImageStore()
     
     if let navigationController = window?.rootViewController as? UINavigationController,
@@ -27,9 +34,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return true
   }
   
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    print(#function)
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+    print(#function)
+  }
+  
+  func applicationWillEnterForeground(_ application: UIApplication) {
+    print(#function)
+  }
+  
   func applicationDidEnterBackground(_ application: UIApplication) {
     let result = itemStore.save()
     assert(result)
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+    print(#function)
   }
 }
 

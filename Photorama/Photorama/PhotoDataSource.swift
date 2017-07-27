997de36524c7d5456aee33f150930c2a2ff3f761
Jason <quietjason@gmail.com>
2017-07-27 16:54:41 +0800

    Add class PhotoDataSource.

---
+++ Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Jason Zheng on 2017/07/27.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+  
+  var photos = [Photo]()
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return photos.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let photoViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoViewCell", for: indexPath)
+    return photoViewCell
+  }
+}

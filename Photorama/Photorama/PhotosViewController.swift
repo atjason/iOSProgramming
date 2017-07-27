84492d77f196b3a7d8134a94eb88b5f144d8ff03
Jason <quietjason@gmail.com>
2017-07-27 08:14:00 +0800

    Add class PhotosViewController

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Jason Zheng on 2017/07/27.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+}

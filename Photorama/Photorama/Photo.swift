504b4cd688fd88f7938e40498286ba7fa11bc053
Jason <quietjason@gmail.com>
2017-07-27 10:23:54 +0800

    Add class Photo.

---
+++ Photorama/Photorama/Photo.swift
@@ -0,0 +1,19 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Jason Zheng on 2017/07/27.
+//  Copyright © 2017 Jason Zheng. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+  var id = ""
+  var title = ""
+  
+  init(id: String, title: String) {
+    self.id = id
+    self.title = title
+  }
+}
